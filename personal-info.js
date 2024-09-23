83b60acdd2e674b14b9ffd57fb67a0c4936ab8e1
MRizwanAnsar <50844550+MRizwanAnsar@users.noreply.github.com>
2024-09-23 20:01:06 +0500

    remove inof

--- personal-info.js
+++ personal-info.js
@@ -1,8 +1,1 @@
-var personalInfo = {
-    "address": "Gulshan Park, Nashat Colony, Lahore Cantt, Pakistan",
-    "phone": "+92 308 4917561",
-    "email":"rizwanansar5@gmail.com",
-    "skype":"live:rizwanansar5",
-    "linkedin":"https://www.linkedin.com/in/rizwan-ansar-9a911a128/"    
-
-}
+var personalInfo = undefined;

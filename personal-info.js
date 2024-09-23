49cbeb2da75aff15b3f8875832df30a1a63dc445
MRizwanAnsar <50844550+MRizwanAnsar@users.noreply.github.com>
2024-09-23 19:59:25 +0500

    added logic of perosonal info.

---
+++ personal-info.js
@@ -0,0 +1,8 @@
+var personalInfo = {
+    "address": "Gulshan Park, Nashat Colony, Lahore Cantt, Pakistan",
+    "phone": "+92 308 4917561",
+    "email":"rizwanansar5@gmail.com",
+    "skype":"live:rizwanansar5",
+    "linkedin":"https://www.linkedin.com/in/rizwan-ansar-9a911a128/"    
+
+}
